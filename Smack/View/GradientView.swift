5cf2d1f1304fea3eca11b83bba6c5b4abeb0105c
Adrian Setniewski <274433@pw.edu.pl>
2018-03-16 14:13:02 +0100

    Added GradientView, and filled the Channel View.

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,39 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Adrian Setniewski on 16.03.2018.
+//  Copyright © 2018 Adrian Setniewski. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class GradientView: UIView {
+
+    // Adjustable in editor.
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.2901960784, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        // Add layer as first.
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        
+    }
+    
+
+}
