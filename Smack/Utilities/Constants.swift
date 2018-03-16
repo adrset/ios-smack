469ecb414967210e7fdae7de3dbaa1ddb7acb690
Adrian Setniewski <274433@pw.edu.pl>
2018-03-16 20:49:13 +0100

    Implementing user registration.

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+// Url
+let BASE_URL = "http://parala.xyz:42313/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+// typedef
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 // Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
