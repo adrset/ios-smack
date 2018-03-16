d9e510d79e99231bdb0c7f5a900e7269069d58f1
Adrian Setniewski <274433@pw.edu.pl>
2018-03-16 14:50:32 +0100

    Added LoginVC and creted login screen.

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Adrian Setniewski on 16.03.2018.
+//  Copyright © 2018 Adrian Setniewski. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Adrian Setniewski on 16.03.2018.
+//  Copyright © 2018 Adrian Setniewski. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
